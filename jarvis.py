53f11d7bf45a0d22d36124730860853e83df6fa9
unknown <klleung72781@gmail.com>
2020-10-06 18:10:04 -0400

    Initial framework

---
+++ jarvis.py
@@ -0,0 +1,16 @@
+# define modules to import
+import os
+import numpy as np
+import pandas as pd
+import ccxt
+import asyncio
+import hvplot.pandas
+import panel as pn
+pn.extension()
+
+# NLP on earnings transcripts of TSLA
+## Find sources of transcripts - King/George
+
+## NLP classification - King/George
+
+## 3 - Ticker tweets NLP (Good2Have) - King
