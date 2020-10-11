99ea8456af812d62f7cab7c64242eb0322281ac0
unknown <klleung72781@gmail.com>
2020-10-11 02:58:15 -0400

    Added streaming function

--- jarvis.py
+++ jarvis.py
@@ -7,16 +7,27 @@ import asyncio
 import hvplot.pandas
 import nltk
 import requests
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api import StreamConn
+from joblib import load
 import re
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from bs4 import BeautifulSoup as bs
 import pyarrow
+import asyncio
+import sys
 import panel as pn
 pn.extension()
 from dotenv import load_dotenv
 load_dotenv()
 nltk.download('vader_lexicon')
 
+# initiate trade api
+api = tradeapi.REST(
+    os.getenv('ALPACA_API_KEY'),
+    os.getenv('ALPACA_SECRET_KEY'),
+    base_url='https://paper-api.alpaca.markets'
+)
 
 # NLP on earnings transcripts of TSLA
 ## Find sources of transcripts - transcripts are downloaded in ./transcripts
@@ -99,4 +110,47 @@ def tweet_search_by_user(handle, count=10):
         df.write_feather(file_name)
     
     return df
-    
+
+
+# main streaming connection - King
+def main(args):
+    print(f'Start Streaming ticker: {args[0]}')
+
+    conn = StreamConn(
+        os.getenv('ALPACA_API_KEY'),
+        os.getenv('ALPACA_SECRET_KEY'),
+        base_url='https://paper-api.alpaca.markets'
+    )
+
+    @conn.on(r'^trade_updates$')
+    async def on_account_updates(conn, channel, account):
+        print('account', account)
+
+    @conn.on(r'^status$')
+    async def on_status(conn, channel, data):
+        print('polygon status update', data)
+
+    @conn.on(r'^AM$')
+    async def on_minute_bars(conn, channel, bar):
+        print('bars', bar)
+
+    @conn.on(r'^A$')
+    async def on_second_bars(conn, channel, bar):
+        print('bars', bar)
+
+    async def periodic():
+        while True:
+            if api.get_clock().is_open == False:
+                print('Market is not open, exiting...')
+                sys.exit(1)
+            await asyncio.sleep(30)
+            
+    loop = conn.loop
+    loop.run_until_complete(asyncio.gather(
+        conn.subscribe(['trade_updates', f'AM.{args[0]}']),
+        periodic(),
+    ))
+    loop.close()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
